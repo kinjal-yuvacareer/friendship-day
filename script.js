9937d46edde88bbe07af051521126bc31ef176a3
kinjal-yuvacareer <kinjalsolanki534@gmail.com>
2026-08-02 13:29:35 +0530

    Add playMusic function to control background music

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function surprise(){
 alert("💖 Happy Friendship Day Paralu!\n\nThank you for being such an amazing friend.\n- Love, Kinjal ❤️");
 }
+function playMusic(){
+    const audio = document.getElementById("bgmusic");
+    if (audio) {
+        audio.volume = 0.4;
+        audio.play().catch(() => {
+            console.log("Music will start after you click once.");  
+        });
+    }
+}
